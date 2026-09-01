b6ebf468c03386b08b5ba2a11cd629480fd43bde
abdulraheemnohri <31163557+abdulraheemnohri@users.noreply.github.com>
2026-09-01 16:43:16 +0500

    feat: Update Color.kt with modern Material 3 palette

--- app/src/main/java/com/videoplay/browser/ui/theme/Color.kt
+++ app/src/main/java/com/videoplay/browser/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package com.videoplay.browser.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Primary Colors
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -9,3 +10,59 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Custom Modern Colors
+val Primary = Color(0xFF6200EE) // Deep Purple
+val PrimaryContainer = Color(0xFFEADDFF)
+val OnPrimary = Color(0xFFFFFFFF)
+
+val Secondary = Color(0xFF03DAC6) // Teal
+val SecondaryContainer = Color(0xFFCCE8E7)
+val OnSecondary = Color(0xFF000000)
+
+val Tertiary = Color(0xFF3700B3) // Dark Purple
+val TertiaryContainer = Color(0xFFE8DDFF)
+val OnTertiary = Color(0xFFFFFFFF)
+
+val Error = Color(0xFFB00020)
+val ErrorContainer = Color(0xFFFFDAD6)
+val OnError = Color(0xFFFFFFFF)
+
+val Background = Color(0xFFFFFBFE)
+val OnBackground = Color(0xFF1C1B1F)
+
+val Surface = Color(0xFFFFFBFE)
+val OnSurface = Color(0xFF1C1B1F)
+
+val SurfaceVariant = Color(0xFFE7E0EC)
+val OnSurfaceVariant = Color(0xFF49454F)
+
+val Outline = Color(0xFF79747E)
+
+// Dark Theme Colors
+val DarkPrimary = Color(0xFFD0BCFF)
+val DarkPrimaryContainer = Color(0xFF4F378B)
+val DarkOnPrimary = Color(0xFF000000)
+
+val DarkSecondary = Color(0xFF86E8DE)
+val DarkSecondaryContainer = Color(0xFF004E49)
+val DarkOnSecondary = Color(0xFF000000)
+
+val DarkTertiary = Color(0xFFCBCAFF)
+val DarkTertiaryContainer = Color(0xFF4A3F8C)
+val DarkOnTertiary = Color(0xFF000000)
+
+val DarkError = Color(0xFFFFB4AB)
+val DarkErrorContainer = Color(0xFF93000A)
+val DarkOnError = Color(0xFF000000)
+
+val DarkBackground = Color(0xFF1C1B1F)
+val DarkOnBackground = Color(0xFFE7E0EC)
+
+val DarkSurface = Color(0xFF1C1B1F)
+val DarkOnSurface = Color(0xFFE7E0EC)
+
+val DarkSurfaceVariant = Color(0xFF49454F)
+val DarkOnSurfaceVariant = Color(0xFFCAC4D0)
+
+val DarkOutline = Color(0xFF938F99)
